9944649e8f0d5b879a9139d95b3eb0fb9b113bcb
Hossain Ayman <ayman.dev2003@gmail.com>
2026-06-10 23:12:55 +0600

    Spring JPA Hibernate Crud project inti

---
+++ src/main/java/com/jpaDemo/curdjpa/CurdjpaApplication.java
@@ -0,0 +1,24 @@
+package com.jpaDemo.curdjpa;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CurdjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdjpaApplication.class, args);
+	}
+    @Bean
+	public CommandLineRunner commandLineRunner(String[]args){
+		return runner ->{
+			System.out.println("=========================================");
+			System.out.println("SUCCESS: The application is actually running.");
+			System.out.println("The Database is connected.");
+			System.out.println("Ready to execute JPA commands.");
+			System.out.println("=========================================");
+		};
+	}
+}
---
+++ src/test/java/com/jpaDemo/curdjpa/CurdjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpaDemo.curdjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
